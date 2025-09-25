6c011507d3ccf312ddde6b7877a29a1beb15e179
Nikita Huliuk <nikaron4ik@gmail.com>
2025-09-25 14:15:48 +0300

    Added .gitignore + almost done 1.0 version of program (need some more fixes + features)

--- Words/Program.cs
+++ Words/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿
 using System.Text.RegularExpressions;
-using System.Timers;
+using Timer = System.Timers.Timer;
 
 class Program
 {
@@ -18,16 +18,31 @@ class Program
             Console.WriteLine("1. Начать игру");
             Console.WriteLine("2. Выход");
             Console.Write("Выберите опцию: ");
-            int option = Convert.ToInt32(Console.ReadLine());
+            string? inp = Console.ReadLine();
+
+            if (inp == null) {
+                Console.WriteLine("Введено некорректное значение!");
+                continue;
+            }
+            int option = Convert.ToInt32(inp);
             switch (option)
             {
                 case 1:
                     {
-                        Console.WriteLine("Выберите язык игры");
+                        Console.WriteLine("Язык игры");
+
                         Console.WriteLine("1. Русский");
                         Console.WriteLine("2. English");
                         Console.WriteLine("3. Вернуться в главное меню");
-                        int subOption = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Выберите опцию: ");
+                        string? subInp = Console.ReadLine();
+
+                        if (subInp == null)
+                        {
+                            Console.WriteLine("Введено некорректное значение!");
+                            continue;
+                        }
+                        int subOption = Convert.ToInt32(subInp);
                         switch (subOption)
                         {
                             case 1:
@@ -50,12 +65,12 @@ class Program
             }
         }
 
-        
+
     }
 
-     enum Language
+    enum Language
     {
-        English, 
+        English,
         Russian
     }
 
@@ -67,10 +82,11 @@ class Program
 
 
     private static string getInitialWord(Language language)
-    {
+    {   
         Regex englishLetters = new Regex(@"^[a-zA-Z]+$");
         Regex russianLetters = new Regex(@"^[а-яА-Я]+$");
 
+        Console.WriteLine("Введите начальное слово:");
         string? word = Console.ReadLine();
 
         if (word == null) return language == Language.Russian ? "абракадабра" : "pronunciation";
@@ -91,28 +107,126 @@ class Program
             }
             return "pronunciation";                     // TODO: также затычка, позже придумать, как грамотнее обработать
         }
-    } 
+    }
 
 
 
     private static void startGame(String startWord)
     {
-        Dictionary<char, int> startWordLetters = new Dictionary<char, int>();
+        Dictionary<char, int> startWordLetters = divideWordIntoLetters(startWord);
+        HashSet<string> usedWords = new HashSet<string>();
+        var player = Player.First;
 
-        foreach (char c in startWord) {
-            if (startWordLetters.ContainsKey(c))
+        while (true)
+        {
+            string playerNum = (player == Player.First ? "1" : "2");
+            Console.WriteLine($"\nХод игрока {playerNum}");
+            Console.WriteLine($"Начальное слово: {startWord}");
+
+            bool isTimeExpired = false;
+            string? input = null;
+
+            Thread inputThread = new Thread(() =>
             {
-                startWordLetters[c]++;
+            try
+                {
+                    Console.WriteLine("Введите слово:");
+                    input = Console.ReadLine();
+                }
+                catch (ThreadInterruptedException) { }
+            });
+
+            Timer timer = new Timer(20000);
+
+            timer.Elapsed += (source, e) =>
+            {
+                inputThread?.Interrupt();
+                isTimeExpired = true;
+                timer.Stop();
+            };
+
+            isTimeExpired = false;
+            timer.Start();
+            inputThread.Start();
+            inputThread.Join();
+
+            timer.Stop();
+
+            /**
+            * Сомнительная проверка, ибо чтобы увидеть сообщение о времени требуется нажать enter, 
+            * т.к. Console.Readline() даже после thread.Interrupt() не заканчивается автоматически,
+            * но пока более адекватного способа прерывания ввода по времени не придумал 
+            */ 
+            if (isTimeExpired)
+            {
+                Console.WriteLine($"К сожалению, время на ввод уже вышло! Игрок {playerNum} проиграл!");
+                break;
+            }
+
+            if (input == null || input == "")
+            {
+                Console.WriteLine($"Строка не была введена! Игрок {playerNum} проиграл!");
+                break;
+            }
+
+            input = input.ToLower();
+
+            if (usedWords.Contains(input))
+            {
+                Console.WriteLine("Слово уже было использовано ранее, придумайте другое!");
+                continue;
+            }
+
+            if (isWordCorrect(input, startWordLetters))
+            {
+                usedWords.Add(input);
+                Console.WriteLine("Слово подходит! Ход переходит к следующему игроку!");
+                player = player == Player.First ? Player.Second : Player.First;
             }
             else
             {
-                startWordLetters.Add(c, 1);
+                Console.WriteLine($"Слово не подходит! Игрок {playerNum} проиграл!");
+                break;
             }
+
         }
 
-        
+
+
 
         return;
     }
+
+    private static Dictionary<char, int> divideWordIntoLetters(string word)
+    {
+        Dictionary<char, int> wordLetters = new Dictionary<char, int>();
+
+        foreach (char c in word.ToLower())
+        {
+            if (wordLetters.ContainsKey(c))
+            {
+                wordLetters[c]++;
+            }
+            else
+            {
+                wordLetters.Add(c, 1);
+            }
+        }
+
+        return wordLetters;
+    }
+
+
+    private static bool isWordCorrect(string input, Dictionary<char, int> startWordLetters)
+    {
+        Dictionary<char, int> inputLetters = divideWordIntoLetters(input);
+
+        foreach (char k in inputLetters.Keys)
+        {
+            if (!(startWordLetters.ContainsKey(k) && startWordLetters[k] >= inputLetters[k])) return false;
+        }
+        return true;
+    }
+    
 
 }
--- Words/obj/Debug/net9.0/Words.AssemblyInfo.cs
+++ Words/obj/Debug/net9.0/Words.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Words")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d87c14e804819ae9aee55fab097c3a57e5501427")]
 [assembly: System.Reflection.AssemblyProductAttribute("Words")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Words")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
