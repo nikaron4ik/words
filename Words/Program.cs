c8a6d05083d0fba8aae50b52e0525a2f4b5af906
Nikita Huliuk <nikaron4ik@gmail.com>
2025-09-28 13:27:59 +0300

    Some refactoring to comply with SRP; comments switched to English lang

--- Words/Program.cs
+++ Words/Program.cs
@@ -5,6 +5,18 @@ using Timer = System.Timers.Timer;
 class Program
 {
 
+    static readonly List<string> DefaultEnglishWords = new List<string>{  "pronunciation", "recognition", "professionality", "specification", "organization", "dependency",
+                                                              "exhibition", "demonstration", "embarrassment", "knowledge", "happiness", "obfuscation",
+                                                              "information", "atmosphere", "characteristics", "misunderstanding", "communication", "accident"};
+    static readonly List<string> DefaultRussianWords = new List<string> { "агрегация", "расширение", "абракадабра", "аббревиатура", "параллелепипед", "раздражение",
+                                                              "прерогатива", "перекрёсток", "пересечение", "уведомление", "разработка", "произношение",
+                                                              "адекватность", "центробежность", "фрустрация", "деобфускация", "заготовка", "интуиция"};
+
+
+    enum Language { English, Russian }
+
+    enum Player { First, Second }
+
 
     static void Main(string[] args)
     {
@@ -13,45 +25,36 @@ class Program
 
         while (true)
         {
-            Console.WriteLine("Игра \"Слова\"!");
+            ShowMainMenu();
+            string? mainMenuInput = Console.ReadLine();
 
-            Console.WriteLine("1. Начать игру");
-            Console.WriteLine("2. Выход");
-            Console.Write("Выберите опцию: ");
-            string? inp = Console.ReadLine();
-
-            if (inp == null) {
+            if (mainMenuInput == null) {
                 Console.WriteLine("Введено некорректное значение!");
                 continue;
             }
-            if (int.TryParse(inp, out int option))
+            if (int.TryParse(mainMenuInput, out int option))
             {
                 switch (option)
                 {
                     case 1:
                         {
-                            Console.WriteLine("Язык игры");
+                            ShowLanguageSelectionMenu();
+                            string? languageInput = Console.ReadLine();
 
-                            Console.WriteLine("1. Русский");
-                            Console.WriteLine("2. English");
-                            Console.WriteLine("3. Вернуться в главное меню");
-                            Console.Write("Выберите опцию: ");
-                            string? subInp = Console.ReadLine();
-
-                            if (subInp == null)
+                            if (languageInput == null)
                             {
                                 Console.WriteLine("Введено некорректное значение!");
                                 continue;
                             }
-                            if (int.TryParse(subInp, out int subOption))
+                            if (int.TryParse(languageInput, out int subOption))
                             {
                                 switch (subOption)
                                 {
                                     case 1:
-                                        startGame(getInitialWord(Language.Russian));
+                                        StartGame(GetInitialWord(Language.Russian));
                                         break;
                                     case 2:
-                                        startGame(getInitialWord(Language.English));
+                                        StartGame(GetInitialWord(Language.English));
                                         break;
                                     case 3:
                                         break;
@@ -80,152 +83,183 @@ class Program
 
     }
 
-    enum Language
+    static void ShowMainMenu()
     {
-        English,
-        Russian
+        Console.WriteLine("Игра \"Слова\"!");
+
+        Console.WriteLine("1. Начать игру");
+        Console.WriteLine("2. Выход");
+        Console.Write("Выберите опцию: ");
     }
 
-    enum Player
+    static void ShowLanguageSelectionMenu()
     {
-        First,
-        Second
+        Console.WriteLine("Язык игры");
+
+        Console.WriteLine("1. Русский");
+        Console.WriteLine("2. English");
+        Console.WriteLine("3. Вернуться в главное меню");
+        Console.Write("Выберите опцию: ");
     }
 
+    static string GetInitialWord(Language language)
+    { 
+        
+        string? word = InitializeWord();
 
-    private static string getInitialWord(Language language)
-    {   
-        List<string> defaultEnglishWords = new List<string>{  "pronunciation", "recognition", "professionality", "specification", "organization", "dependency",
-                                                              "exhibition", "demonstration", "embarrassment", "knowledge", "happiness", "obfuscation", 
-                                                              "information", "atmosphere", "characteristics", "misunderstanding", "communication", "accident"};
-        List<string> defaultRussianWords = new List<string> { "агрегация", "расширение", "абракадабра", "аббревиатура", "параллелепипед", "раздражение",
-                                                              "прерогатива", "перекрёсток", "пересечение", "уведомление", "разработка", "произношение", 
-                                                              "адекватность", "центробежность", "фрустрация", "деобфускация", "заготовка", "интуиция", 
-                                                            };
+        if (IsValidInitialWord(word, language))
+        {
+            return word;
+        }
 
-        Regex englishLetters = new Regex(@"^[a-zA-Z]+$");
-        Regex russianLetters = new Regex(@"^[а-яА-Я]+$");
+        
+        Console.WriteLine("Введённое слово не соответствует условиям, начальное слово будет выбрано случайно!");
+        return GetRandomWord(language);
+        
+    }
 
+    static string? InitializeWord()
+    {
         Console.WriteLine("Введите начальное слово (от 8 до 30 символов):");
-        string? word = Console.ReadLine();
-           
-        if (word == null)
+        return Console.ReadLine();
+    }
+
+    static bool IsValidInitialWord(string? word, Language language)
+    {
+        if (word == null) return false;
+
+        Regex regex = language == Language.Russian 
+            ? new Regex(@"^[a-zA-Z]+$") 
+            : new Regex(@"^[а-яА-Я]+$");
+
+        if (regex.IsMatch(word) && word.Length >= 8 && word.Length <= 30)
         {
-            Random random = new Random();
-            return (language == Language.Russian ? defaultRussianWords[random.Next(defaultRussianWords.Count)] : defaultEnglishWords[random.Next(defaultEnglishWords.Count)]);
+            return true;
         }
 
-        if (language == Language.Russian)
-        {
-            if (russianLetters.IsMatch(word) && (word.Length >= 8 && word.Length <= 30))
-            {
-                return word;
-            }
-            Random random = new Random();
-            Console.WriteLine("Введённое слово не соответствует условиям, начальное слово будет выбрано случайно!");
-            return defaultRussianWords[random.Next(defaultRussianWords.Count)];
-        }
-        else
-        {
-            if (englishLetters.IsMatch(word) && (word.Length >= 8 && word.Length <= 30))
-            {
-                return word;
-            }
-            Random random = new Random();
-            Console.WriteLine("Введённое слово не соответствует условиям, начальное слово будет выбрано случайно!");
-            return defaultEnglishWords[random.Next(defaultEnglishWords.Count)];
-        }
+        return false;
+    }
+
+    static string GetRandomWord(Language language)
+    {
+        Random random = new Random();
+        return language == Language.Russian 
+            ? DefaultRussianWords[random.Next(DefaultRussianWords.Count)] 
+            : DefaultEnglishWords[random.Next(DefaultEnglishWords.Count)];
     }
 
 
 
-    private static void startGame(String startWord)
+    static void StartGame(String startWord)
     {
-        Dictionary<char, int> startWordLetters = divideWordIntoLetters(startWord);
+        Dictionary<char, int> startWordLetters = DivideWordIntoLetters(startWord);
         HashSet<string> usedWords = new HashSet<string>();
         var player = Player.First;
 
         while (true)
         {
-            string playerNum = (player == Player.First ? "1" : "2");
-            Console.WriteLine($"\nХод игрока {playerNum}");
-            Console.WriteLine($"Начальное слово: {startWord}");
+            ShowPlayerTurn(player, startWord);
 
-            bool isTimeExpired = false;
-            string? input = null;
+            (string? word, bool isTimeOut) playerInput = HandlePlayerInputWithTimer();
 
-            Thread inputThread = new Thread(() =>
+            if (playerInput.isTimeOut == true || (playerInput.word == null  || playerInput.word == ""))
             {
-            try
-                {
-                    Console.WriteLine("Введите слово:");
-                    input = Console.ReadLine();
-                }
-                catch (ThreadInterruptedException) { }
-            });
-
-            Timer timer = new Timer(20000);
-
-            timer.Elapsed += (source, e) =>
-            {
-                inputThread?.Interrupt();
-                isTimeExpired = true;
-                timer.Stop();
-            };
-
-            isTimeExpired = false;
-            timer.Start();
-            inputThread.Start();
-            inputThread.Join();
-
-            timer.Stop();
-
-            /**
-            * Сомнительная проверка, ибо чтобы увидеть сообщение о времени требуется нажать enter, 
-            * т.к. Console.Readline() даже после thread.Interrupt() не заканчивается автоматически,
-            * но пока более адекватного способа прерывания ввода по времени не придумал 
-            */ 
-            if (isTimeExpired)
-            {
-                Console.WriteLine($"К сожалению, время на ввод уже вышло! Игрок {playerNum} проиграл!");
+                TimeOutOrNotCorrectWordLossMessage(player, playerInput.isTimeOut);
                 break;
             }
 
-            if (input == null || input == "")
-            {
-                Console.WriteLine($"Строка не была введена! Игрок {playerNum} проиграл!");
-                break;
-            }
+            string word = playerInput.word.ToLower();
 
-            input = input.ToLower();
-
-            if (usedWords.Contains(input))
+            if (usedWords.Contains(word))
             {
                 Console.WriteLine("Слово уже было использовано ранее, придумайте другое!");
                 continue;
             }
 
-            if (isWordCorrect(input, startWordLetters))
+            if (IsWordCorrect(word, startWordLetters))
             {
-                usedWords.Add(input);
+                usedWords.Add(word);
                 Console.WriteLine("Слово подходит! Ход переходит к следующему игроку!");
-                player = player == Player.First ? Player.Second : Player.First;
+                player = SwitchPlayer(player);
             }
             else
             {
-                Console.WriteLine($"Слово не подходит! Игрок {playerNum} проиграл!");
+                Console.WriteLine($"Слово не подходит! Игрок {GetCurrentPlayer(player)} проиграл!");
                 break;
             }
 
         }
 
-
-
-
         return;
     }
 
-    private static Dictionary<char, int> divideWordIntoLetters(string word)
+    /*
+     *  Questionable checking, because in order to see time expired message user need to press "Enter" button,
+     *  cuz Console.Readline() doesn't end automatically even after thread.Interrput(),
+     *  but I don't know how to fix it properly right now, maybe will find any good way later
+    */
+    static (string? input, bool isTimeOut) HandlePlayerInputWithTimer()
+    {
+        bool isTimeOut = false;
+        string? input = null;
+
+        Thread inputThread = new Thread(() =>
+        {
+            try
+            {
+                Console.WriteLine("Введите слово:");
+                input = Console.ReadLine();
+            }
+            catch (ThreadInterruptedException) { }
+        });
+
+        Timer timer = new Timer(20000);
+
+        timer.Elapsed += (source, e) =>
+        {
+            inputThread?.Interrupt();
+            isTimeOut = true;
+            timer.Stop();
+        };
+
+        isTimeOut = false;
+        timer.Start();
+        inputThread.Start();
+        inputThread.Join();
+
+        timer.Stop();
+
+        return (input, isTimeOut);
+    }
+
+    static void ShowPlayerTurn(Player player, string startWord)
+    {
+        string playerNum = (player == Player.First ? "1" : "2");
+        Console.WriteLine($"\nХод игрока {playerNum}");
+        Console.WriteLine($"Начальное слово: {startWord}");
+    }
+
+    static string GetCurrentPlayer(Player player)
+    {
+        return player == Player.First ? "1" : "2";
+    }
+
+    static Player SwitchPlayer(Player player)
+    {
+        return player == Player.First ? Player.Second : Player.First;
+    }
+
+    static void TimeOutOrNotCorrectWordLossMessage(Player player, bool isTimeOut)
+    {
+        if (isTimeOut)
+        {
+            Console.WriteLine($"К сожалению, время на ввод уже вышло! Игрок {GetCurrentPlayer(player)} проиграл!");
+        }
+
+        Console.WriteLine($"Строка не была введена! Игрок {GetCurrentPlayer(player)} проиграл!");
+    }
+
+    static Dictionary<char, int> DivideWordIntoLetters(string word)
     {
         Dictionary<char, int> wordLetters = new Dictionary<char, int>();
 
@@ -245,9 +279,9 @@ class Program
     }
 
 
-    private static bool isWordCorrect(string input, Dictionary<char, int> startWordLetters)
+    static bool IsWordCorrect(string input, Dictionary<char, int> startWordLetters)
     {
-        Dictionary<char, int> inputLetters = divideWordIntoLetters(input);
+        Dictionary<char, int> inputLetters = DivideWordIntoLetters(input);
 
         foreach (char k in inputLetters.Keys)
         {
